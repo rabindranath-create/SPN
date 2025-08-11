e3f26a62e09beacb55583f4c6f3bdc0b4c5397e4
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2025-08-11 13:11:50 -0500

    Add files via upload

--- main_script41.R
+++ main_script41.R
@@ -556,7 +556,7 @@ Simple_Node_Eliminate_modified2 <- function(G, s, t, W){
 alpha <- 41
 
 
-obs_info_all1 <- read.csv('obs_info_all_20.csv')
+obs_info_all1 <- read.csv('obs_info_all_40.csv')
 
 
 
@@ -715,8 +715,8 @@ W <- 6
 
 
 result_WCSPP_risk_30 <- matrix(NA,ncol=7,nrow=100)
-write.csv(result_WCSPP_risk_30, file = file.path(output_dir, paste0("result_WCSPP_risk_", alpha, "_20_1.csv")))
-for (i in 1:100){
+write.csv(result_WCSPP_risk_30, file = file.path(output_dir, paste0("result_WCSPP_risk_", alpha, "_40_1.csv")))
+for (i in 1:50){
     obs_info_all_use <- obs_info_all[[i]]
     result <- WCSPP_Node_risk_30(obs_info_all_use)
     result_WCSPP_risk_30[i, 1] <- result$Length_total
@@ -726,7 +726,7 @@ for (i in 1:100){
     result_WCSPP_risk_30[i,5] <- result$LU_diff[2]
     result_WCSPP_risk_30[i,6] <- result$LU_diff[3]
     result_WCSPP_risk_30[i,7] <- result$LU_diff[4]
-    write.csv(result_WCSPP_risk_30, file = file.path(output_dir, paste0("result_WCSPP_risk_", alpha, "_20_1.csv")))
+    write.csv(result_WCSPP_risk_30, file = file.path(output_dir, paste0("result_WCSPP_risk_", alpha, "_40_1.csv")))
   }
 
 
