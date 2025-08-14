f35abdb297a09e1e7cb357a403d177374108abe8
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2025-08-14 11:00:08 -0500

    Add files via upload

--- main_script5.R
+++ main_script5.R
@@ -596,7 +596,7 @@ Update_graph_intersect<-function(g,x,y,circle_info,r){
 
 
 WCSPP_Node_risk_30 <- function(obs_info){
-W <- 6
+W <- 4
   x <- 100; y <- 50; r <- 5
   # create graph
   vertice_list <- Lattice_Vertices(x,y)
